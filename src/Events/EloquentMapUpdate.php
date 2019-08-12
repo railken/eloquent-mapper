80f2aa63a600ddd03a9d2a1d5a65e5f2a2cb27b3
railken <railken@protonmail.ch>
2019-08-12 15:56:14 +0200

    model name event

--- src/Events/EloquentMapUpdate.php
+++ src/Events/EloquentMapUpdate.php
@@ -4,4 +4,16 @@ namespace Railken\EloquentMapper\Events;
 
 class EloquentMapUpdate
 {
+	/**
+	 * @var string
+	 */
+	public $model;
+
+	/**
+	 * @param string $model
+	 */
+	public function __construct(string $model)
+	{
+		$this->model = $model;
+	}
 }

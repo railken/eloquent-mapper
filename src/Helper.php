96bcd3d168ea9b59a66d209120c9e8b0bf6cb8f7
railken <railken@protonmail.ch>
2019-10-19 17:40:12 +0200

    remove inverse

--- src/Helper.php
+++ src/Helper.php
@@ -15,20 +15,6 @@ class Helper
         $this->initializeStorage();
 
         $this->finder = new Finder($this->getRelationsByStorage());
-
-        // Inversed relationships must be reloaded.
-        foreach ($this->finder->data() as $model => $relations) {
-
-            if (!class_exists($model)) {
-
-                // not valid anymore.
-                $this->removeRelationsByStorage();
-                $this->finder->removeData($model);
-
-            } else {
-                $this->defineInverseRelationship($relations, $model);
-            }
-        };
     }
 
     public function getFinder(): Finder
@@ -46,10 +32,6 @@ class Helper
 
     public function generate(array $models)
     {
-        foreach ($models as $model) {
-            $this->defineInverseRelationship(Mapper::relations($model), $model);
-        }
-
         foreach ($models as $model) {
             $this->generateModel($model);
         }
@@ -110,42 +92,6 @@ class Helper
         $content[$model] = $this->getRelations($model)->toArray();
 
         $this->setRelationsStorage($content);
-    }
-
-    public function defineInverseRelationship(array $relations, string $model)
-    {
-        $morphName = $model::getStaticMorphName();
-
-        collect($relations)->map(function ($relation, $key) use ($model, $morphName) {
-
-            $related = $relation->model;
-
-            if (!method_exists($related, 'getDynamicRelations')) {
-                return;
-            }
-
-            $inflector = new \Doctrine\Common\Inflector\Inflector;
-            $methodPlural = $inflector->camelize(Str::plural($morphName));
-
-            if (!method_exists($related, $methodPlural)) {
-                if ($relation->type === 'BelongsTo') {
-                    $related::has_many($methodPlural, $model);
-                }
-
-                if ($relation->type === 'MorphToMany') {
-                    if (isset($relation->morphType)) {
-                        $key = str_replace('_type', '', $relation->morphType);
-
-                        $newRelation = $related::morphed_by_many($methodPlural, $model, $key, $relation->table, $relation->relatedPivotKey, $relation->foreignPivotKey)
-                            ->using($relation->intermediate);
-
-                        foreach (array_slice($relation->scope, 1) as $scope) {
-                            $newRelation->where($scope['column'], $scope['operator'], $scope['value']);
-                        }
-                    }
-                }
-            }
-        });
     }
 
     public function getFilePath()
